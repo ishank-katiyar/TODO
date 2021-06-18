2f9c221bdc2dc2b3beff676c32cebfc795dd44af
ishank-katiyar <ishankkatiyar162@gmail.com>
2021-06-18 15:42:48 +0530

    non-initial

--- backend/config/config.js
+++
@@ -1,9 +0,0 @@
-const development = {
-	API_ENDPOINT: "/api/",
-	REFRESH: "getAll",
-	CREATE_ENTRY: "createEntry",
-	UPDATE_ENTRY: "updateEntry",
-	DELETE_ENTRY: "deleteEntry",
-};
-
-module.exports = development;
--- frontend/src/config/config.js
+++
@@ -1,9 +0,0 @@
-const development = {
-	API_ENDPOINT: "http://localhost:5000/api/",
-	REFRESH: "getAll",
-	CREATE_ENTRY: "createEntry",
-	UPDATE_ENTRY: "updateEntry",
-	DELETE_ENTRY: "deleteEntry",
-};
-
-module.exports = development;
